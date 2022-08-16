62da6ef427c9fb31f6aac60a16824db441420f1f
Prihod-Sveta <svetasuzuki@mail.ru>
2022-08-16 22:31:41 +0300

    Lesson 3

---
+++ Lesson 3/task dop/Program.cs
@@ -0,0 +1,27 @@
+﻿// Доп. задача
+// Написать программу для разворота массива
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    int index = 0;
+    while (index < count)
+    {
+        Console.Write(arr[index] + " ");
+        index++;
+    }
+}
+int[] arr = { 48, 75, 13, 94, 50, 14 };
+int N = arr.Length;
+PrintArray(arr);
+Console.WriteLine();
+// var str = string.Join(" ", arr);
+// Console.WriteLine(str);
+
+for (int i = 0; i < N / 2; i++)
+{
+    int temp = arr[i];
+    arr[i] = arr[N - i - 1];
+    arr[N - i - 1] = temp;
+}
+
+PrintArray(arr);
